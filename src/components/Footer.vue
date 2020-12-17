5cac2458f195291e16572b5d1b9fafc8dd96be62
Goizane <goizaneterreros@gmail.com>
2020-12-17 13:15:54 +0100

    Plantilla - Part 1

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <h5>@Grupo 3 - 2DW3 - CIFP Txurdinaga</h5>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav>
+        <img src="logoDF.png" alt="logo" style="border-radius:50%">
+        <a href="">Inicio</a>
+        <a href="">Blog</a>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
