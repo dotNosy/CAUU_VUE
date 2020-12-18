c6b69414b69c29a41cd02b13e575a1c4094081ed
davidfpweb <71374850+davidfpweb@users.noreply.github.com>
2020-12-18 12:36:11 +0100

    HEADER Y FOOTER
    HEADER Y FOOTER

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
-    <h5>@Grupo 3 - 2DW3 - CIFP Txurdinaga</h5>
+    <footer class="text-center text-lg-start justify-content-center mt-auto"  style="background-color: #2d2c2f">
+    <!-- Copyright -->
+    <div class="divfooter text-center p-3">
+    © 2020 Copyright:
+    <a class="text-light" href="https://www.youtube.com/watch?v=6TolbTZXDjI&ab_channel=ぽにきゃん-AnimePONYCANYONhttps://www.youtube.com/watch?v=6TolbTZXDjI&ab_channel=ぽにきゃん-AnimePONYCANYON">Desayunos Feministas</a>
+    </div>
+    <!-- Copyright -->
+</footer>
 </template>
 
 <script>
@@ -7,3 +14,15 @@
         
     }
 </script>
+
+<style>
+footer {
+    position:absolute;
+    bottom:0;
+    width:100%;
+}
+.divfooter{
+    color: white;
+}
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,58 @@
 <template>
-    <nav>
-        <img src="../assets/logoDF.png" alt="logo" style="border-radius:50%">
-        <a href="">Inicio</a>
-        <a href="">Blog</a>
-    </nav>
+<header>
+<nav class="navbar navbar-expand-sm" style="background-color: #2d2c2f">
+    <a href="./home" class="logo">
+    <img src="../assets/logoDF.png" alt="logo" class="imglogo">
+</a>
+    <ul class="navbar-nav">
+    <li class="nav-item" style="margin-left: 8%">
+        <button type="button" class="btn btn-primary"><a class="nav-link text-light" href="#">Link1</a></button>
+    </li>
+    <li class="nav-item">
+        <button type="button" class="btn btn-primary"><a class="nav-link text-light" href="#">Link2</a></button>
+    </li>
+    <li class="nav-item">
+        <button type="button" class="btn btn-primary"><a class="nav-link text-light" href="#">Link3</a></button>
+    </li>
+    <li class="nav-item">
+        <button type="button" class="btn btn-primary" style="position: absolute; right: 0; margin-right:1%;"><a class="nav-link text-light" href="#">Login</a></button>
+    </li>
+
+    </ul>
+</nav>
+</header>
 </template>
+
+<style>
+.logo{
+    width: 4%;
+}
+.imglogo{
+    width:100%;
+    border-radius: 50%;
+}
+li{
+    margin-left: 1%;
+    margin-right: 1%;
+    list-style: none;
+    display: inline;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-size: 1.3em;
+    padding-left: 2%;
+    padding-right: 2%;
+}
+ul{
+    display: inline;
+}
+.btn{
+    background-color: #4e3757;
+    border-color:#4e3757;
+}
+.btn:hover{
+    background-color: #918897;
+    border-color: #918897;
+}
+</style>
 
 <script>
     export default {
