3e2c0316b7a0ef0c2ff1b1cb6f62ec68eedd2a81
Goizane <goizaneterreros@gmail.com>
2021-01-18 12:45:50 +0100

    La página obliga a los usuarios del movil a jugar en vertical y no permite girar la pantalla

--- src/components/Plantilla.vue
+++ src/components/Plantilla.vue
@@ -22,6 +22,18 @@
         'my-arriba': Arriba
         }
     }
+
+    // Orientacion fijada en vertical para todo navegador en el que se abra
+    // TO-DO TEST EN REAL   
+    const orientation = "portrait";
+
+    ScreenOrientation.lock(orientation); //Android
+    screen.mozLockOrientation(orientation); //Mozzila
+    Screen.orientation.lock(orientation); // Chrome, Edge, Firefox, Internet Explorer, Opera, Android Chrome/Firefox and Samsung internet
+    // window.screen.lockOrientation(orientation); //
+    // screen.msLockOrientation(orientation); //
+
+
 </script>
 
 <style>
